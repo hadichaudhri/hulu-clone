3abf290bcf3e69d839a530e718493724274e7101
Hadi Chaudhri <45558941+hadichaudhri@users.noreply.github.com>
2022-10-03 17:22:02 -0400

    ListingItem: Add skeleton loading and fallback image

--- components/ListingItem.tsx
+++ components/ListingItem.tsx
@@ -1,16 +1,24 @@
 import Movie from '../types/Movie'
 import Image from 'next/image'
+import { useState } from 'react'
 
 const ListingItem = (listing: Movie): JSX.Element => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [imgSrc, setImgSrc] = useState(listing.backdrop_path)
+
   return (
     <>
       <div className="relative mb-2 h-40 transition-transform delay-150 duration-200 hover:z-10 hover:scale-[1.04]">
         <Image
-          className="cursor-pointer rounded-xl bg-turquoise-400"
+          className={`cursor-pointer rounded-xl bg-turquoise-300 ${
+            isLoading && 'animate-pulse'
+          }`}
           alt={`${listing.title} Poster`}
-          src={listing.backdrop_path}
+          src={imgSrc}
           objectFit="cover"
           layout="fill"
+          onLoadingComplete={() => setIsLoading(false)}
+          onErrorCapture={() => setImgSrc(listing.poster_path)}
         />
       </div>
       <span className="line-clamp-1">{listing.title}</span>
