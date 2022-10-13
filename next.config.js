254eaeffa1f1656885f9700b16f8e3bd7940ea40
Hadi Chaudhri <hadichaudhri@gmail.com>
2022-10-13 07:37:26 -0400

    Add bundle-analyzer next plugin to see bundle size

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   images: {
     domains: ['press.hulu.com', 'images.tmdb.org'],
   },
-}
+  reactStrictMode: true,
+})
